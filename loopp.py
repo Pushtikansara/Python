4f0141d20736fd767b36319f379a21549fd5b02c
Pushtikansara <pushti612@gmail.com>
2025-06-10 16:08:34 +0530

    final commit

---
+++ loopp.py
@@ -0,0 +1,11 @@
+name="apple"
+for i in name:
+   print(i)
+fruit=["apple","mango","banana"]
+for name in fruit:
+    print(name)
+    for i in name:
+        print(i)   
+
+for i in range(0,14):
+    print(i+1)
