a768926cf5605be645e7b42715f86dddaa4cc1ae
Pushtikansara <pushti612@gmail.com>
2025-06-12 22:46:31 +0530

    final commit

---
+++ tublee.py
@@ -0,0 +1,12 @@
+tup=(1)
+# A single value without a comma is not a tuple, it's just an integer
+print(type(tup))
+print(tup)      
+
+tup=('1',)
+#A single value tuple must have a comma after the value
+print(type(tup))
+print(tup)      
+
+tup1=(1, 2, 3, 4, 5)
+print(tup1[0])
