e494537b6055bb47c853088bf9eca35edc56920b
Pushtikansara <pushti612@gmail.com>
2025-06-20 21:56:07 +0530

    final commit

---
+++ Basic/dicmethod.py
@@ -0,0 +1,11 @@
+Student1 = {
+    12: "Harry",
+    14: "Shivam",
+    13: "Rohan",
+     
+}
+Student2 ={
+    10: "Shivani",
+    11: "Sakshi"    }
+Student1.update(Student2)  # This will merge Student2 into Student1
+print(Student1)  # This will print the updated dictionary with all key-value pairs
