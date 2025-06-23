ff724e14eaf2fde36fc150bc184e315290218844
Pushtikansara <pushti612@gmail.com>
2025-06-23 18:50:02 +0530

    final commit

--- Basic/loop.py
+++ Basic/loop.py
@@ -6,11 +6,11 @@ for i in range(5):
 else:
     print("Loop completed")
 
-i=0
-while i < 5:
+j=1
+while j <7:
 
-    print(i)
-    if i == 3:
-        print("Breaking the loop at i =", i)
-        break
-    i += 1
+    print(j)
+    if j == 5:
+       print("Breaking the loop at j =", j)
+       break
+    j += 1
